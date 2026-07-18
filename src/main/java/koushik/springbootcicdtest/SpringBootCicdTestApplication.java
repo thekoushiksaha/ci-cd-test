87cbd5696c5f751585d87d2b5bf4111f0d62f8a4
Koushik Saha <thekoushiksaha@zohomail.in>
2026-07-18 18:50:08 +0530

    Initialized

---
+++ src/main/java/koushik/springbootcicdtest/SpringBootCicdTestApplication.java
@@ -0,0 +1,13 @@
+package koushik.springbootcicdtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootCicdTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootCicdTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/koushik/springbootcicdtest/SpringBootCicdTestApplicationTests.java
@@ -0,0 +1,13 @@
+package koushik.springbootcicdtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootCicdTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
