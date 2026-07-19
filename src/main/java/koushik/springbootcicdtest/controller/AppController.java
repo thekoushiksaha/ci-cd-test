69b02a21a35e06cc74e4f26325cdf1687d658d60
Koushik Saha <thekoushiksaha@zohomail.in>
2026-07-19 22:04:03 +0530

    v1.0.2

--- src/main/java/koushik/springbootcicdtest/controller/AppController.java
+++ src/main/java/koushik/springbootcicdtest/controller/AppController.java
@@ -1,8 +1,12 @@
 package koushik.springbootcicdtest.controller;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/v1")
@@ -10,5 +14,13 @@ public class AppController {
     @GetMapping()
     public String helloWorld() {
         return "Hello World from Koushik";
+    }
+
+    @GetMapping("/info")
+    public ResponseEntity<Map<String,String>> getInfo(){
+        Map<String,String> map = new HashMap<>();
+        map.put("App Name","Spring-Boot-CICD-Test");
+        map.put("Developer","Koushik Saha");
+        return ResponseEntity.ok(map);
     }
 }
